2ebac780eeaee6268e767a07b84958c94109e00b
BubbaBean <brandon.runkel@gmail.com>
2017-07-31 15:41:29 -0400

    functionality now works with items being populated to both sections

--- main.js
+++ main.js
@@ -4,38 +4,47 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
-const data = require('./data.js')
+const exphandleBars = require('express-handlebars')
+const data = [];
 const fs = require('fs');
 const app = express();
 
 //Define templates
-app.engine('mustache', mustacheExpress());
+app.engine('handlebars', exphandleBars());
 app.set('views','./views');
-app.set('view engine', 'mustache');
-
-//Define how page serves static files
-app.use(express.static('public'));
-
-//Define page render behaviour
-app.get('/', function(req, res){
-    res.render('home',{
-      data: data
-    })
-  });
+app.set('view engine', 'handlebars');
 
 //Reference form, request body and store into variable, push to data array//
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.post('/todo', function(req, res){
-  let thing_to_do = req.body;
-  data.push(thing_to_do);
+//Define how page serves static files
+app.use(express.static('public'));
 
-  res.render('home', {
-    data: data
-  })
+//Define completed array
+let doneItems = [];
+
+//Define page render behavior
+app.get('/', function(req, res){
+    res.render('home',{
+      data: data,
+      complete: doneItems
+    })
+    console.log(data);
+  });
+
+app.post('/', function(req, res){
+  let thing_to_do = req.body.task;
+  data.push(thing_to_do);
+  res.redirect('/');
 });
 
+app.get('/complete/:index', function(req, res){
+  let thingsDone = data.splice(req.params.index, 1);
+  doneItems.push(thingsDone)
+  console.log(doneItems)
+  res.redirect('/')
+});
 
 app.listen(3000, function(){
   console.log('The program has started successfully!');
